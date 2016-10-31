9766603f9fb68d5c278cd548979fb303c5785056
Noah NU <noahnu@gmail.com>
2016-10-31 18:25:32 -0400

    MYSQL compliant. closed #1

--- server_dev/database/schema.sql
+++ server_dev/database/schema.sql
@@ -1,5 +1,9 @@
-CREATE DATABASE IF NOT EXISTS poppin_schema;
+CREATE SCHEMA IF NOT EXISTS poppin_schema
+	DEFAULT CHARACTER SET utf8
+	DEFAULT COLLATE utf8_unicode_ci;
 USE poppin_schema;
+
+SET default_storage_engine=InnoDB;
 
 
 /* --- Users --- */
@@ -7,24 +11,24 @@ USE poppin_schema;
 /*
 	user
 	
-	id: unique id for user, in sqlite it's an alias for ROWID
-	name: name/nickname/screen name of user, i.e. what to address the user as
+	id: unique id for user
+	first_name: first name of user, i.e. what to address the user as
+	last_name: last name of the user (optional since names may come from 3rd party auth protocols)
 	email: used for communication and in conjunction with login_credential
-	date_created: unix timestamp referring to date account was created
+	date_created: datetime referring to date account was created
 */
 CREATE TABLE IF NOT EXISTS user (
-  id INTEGER PRIMARY KEY,
-  name VARCHAR(255) NOT NULL,
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(255) NOT NULL,
+  last_name VARCHAR(255) NULL,
   email VARCHAR(255) NOT NULL UNIQUE,
-  date_created INTEGER(4) NOT NULL DEFAULT (strftime('%s', 'now'))
+  date_created DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 /*
 	login_credential
 	
-	Users either log in with their email + password,
-	or by using one of the other authentication protocols whose
-	token is stored in login_credential.
+	Log in with email + password.
 	
 	user_id: references user(id)
 	login_attempts: number of failed consecutive login attempts (inherent max at 127)
@@ -32,8 +36,8 @@ CREATE TABLE IF NOT EXISTS user (
 	recovery_token: randomly generated (when needed) token to be used for password recovery
 */
 CREATE TABLE IF NOT EXISTS login_credential (
-  user_id INTEGER PRIMARY KEY,
-  login_attempts INTEGER(1) NOT NULL DEFAULT 0,
+  user_id INT PRIMARY KEY,
+  login_attempts TINYINT NOT NULL DEFAULT 0,
   password VARCHAR(255) NOT NULL,
   recovery_token VARCHAR(255) NULL,
 
@@ -43,12 +47,14 @@ CREATE TABLE IF NOT EXISTS login_credential (
 /*
 	third_party_credential
 	
-	user_id: TODO
-	provider_id: TODO
+	user_id: user id 3rd party credential is associated with
+	provider_id: google/facebook/etc...
+	token: token associated with provider
 */
 CREATE TABLE IF NOT EXISTS third_party_credential (
-  user_id INTEGER NOT NULL,
-  provider_id VARCHAR(32) NOT NULL,
+  user_id INT NOT NULL,
+  provider_id VARCHAR(255) NOT NULL,
+  token VARCHAR(255) NOT NULL,
   
   PRIMARY KEY(user_id, provider_id),
   FOREIGN KEY(user_id) REFERENCES user(id) ON DELETE CASCADE
@@ -63,12 +69,12 @@ CREATE TABLE IF NOT EXISTS third_party_credential (
 	Communities represent interests groups which users subscribe to,
 	and events are associated with.
 	
-	id: unique community id (alias for ROWID)
+	id: unique community id
 	name: unique community name; should be kept short
 	description: short (optional) description explaining the community
 */
 CREATE TABLE IF NOT EXISTS community (
-  id INTEGER PRIMARY KEY,
+  id INT AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(255) NOT NULL UNIQUE,
   description TEXT NULL
 );
@@ -81,32 +87,32 @@ CREATE TABLE IF NOT EXISTS community (
 	
 	id: event id
 	name: short event name
-	start_date: start datetime of event (unix timestamp)
-	end_date: end datetime of event (unix timestamp)
+	start_date: start datetime of event
+	end_date: end datetime of event
 	longitude: longitude of event location
 	latitude: latitude of event location
 	description: description of event
 	is_active: TODO
-	date_created: datetime (unix timestamp) event was created
+	date_created: datetimeevent was created
 */
 CREATE TABLE IF NOT EXISTS event (
-  id INTEGER PRIMARY KEY,
-  name TEXT NOT NULL,
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
   description TEXT NOT NULL,
-  start_date INTEGER(4) NOT NULL,
-  end_date INTEGER(4) NOT NULL,
-  longitude DECIMAL(9,6) NOT NULL,
-  latitude DECIMAL(9,6) NOT NULL,
-  is_active INTEGER(1) NOT NULL,
-  date_created INTEGER(4)
+  start_date DATETIME NOT NULL,
+  end_date DATETIME NOT NULL,
+  longitude FLOAT(10,6) NOT NULL,
+  latitude FLOAT(10,6) NOT NULL,
+  is_active TINYINT(1) NOT NULL,
+  date_created DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 
 /* --- Mappings for events and users with communities --- */
 
 CREATE TABLE IF NOT EXISTS event_community_map (
-  event_id INTEGER NOT NULL,
-  community_id INTEGER NOT NULL,
+  event_id INT NOT NULL,
+  community_id INT NOT NULL,
 
   PRIMARY KEY(event_id, community_id),
   FOREIGN KEY(event_id) REFERENCES event(id) ON DELETE CASCADE,
@@ -114,8 +120,8 @@ CREATE TABLE IF NOT EXISTS event_community_map (
 );
 
 CREATE TABLE IF NOT EXISTS user_community_map (
-  user_id INTEGER NOT NULL,
-  community_id INTEGER NOT NULL,
+  user_id INT NOT NULL,
+  community_id INT NOT NULL,
 
   PRIMARY KEY(user_id, community_id),
   FOREIGN KEY(user_id) REFERENCES user(id) ON DELETE CASCADE,
