c77b6f93acc8c4eb6725e429c92dccb9a57cdb40
anshayghosh <anshay.ghosh@mail.utoronto.ca>
2016-11-08 18:26:54 -0500

    adding event scss and js file

--- client_dev/src/js/views/Event.js
+++ client_dev/src/js/views/Event.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+
+import MapContainer from '../components/MapContainer';
 /* Import Material UI Components. */
 import Container from 'muicss/lib/react/container';
 
@@ -12,9 +14,28 @@ export default class Event extends React.Component {
     }
 
     render() {
+        const text = {
+            title: 'Go Leafs Go',
+            subtitle: "Join us to cheer on the leafs while they win the Stanley cup. Tell your friends about us."
+        };
+
         return (
-            <Container>
-                <div className="message bordered large center">Template Message: Event</div>
+            <Container className='expand'>
+                <div className='event-header'>
+                    <span className='title'>{text['title']}</span>
+                </div>
+                <div className='map-display'>
+                    <MapContainer latitude={38.898556}longitude={-77.037852} />
+                </div>
+                <div className='event-details'>
+                    <span className='subtitle'>{text['subtitle']} </span><br />
+                    <span className='subtitle'>Duration: </span><br />
+                    <span className='date'> 1478045486 - 1478049486 </span><br />
+                    <span className='subtitle'>Distance: </span><br />
+                    <span className='date'> 3.2 </span><br />
+                    <span className='subtitle'>Communities: </span>
+                </div>
+
             </Container>
         );
     }
