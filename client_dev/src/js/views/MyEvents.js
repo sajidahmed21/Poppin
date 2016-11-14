1ba3bb9ede20cd360e96527610f6dc244050af28
NoahNU <noahnu@gmail.com>
2016-11-14 16:02:25 -0500

    close #37

--- client_dev/src/js/views/MyEvents.js
+++ client_dev/src/js/views/MyEvents.js
@@ -3,18 +3,39 @@ import React from 'react';
 /* Import Material UI Components. */
 import Container from 'muicss/lib/react/container';
 
+import EventList from '../components/EventList';
+
 export default class MyEvents extends React.Component {
     constructor() {
         super();
 
         /* Initialize blank state. */
         this.state = {};
+        this.onEventSelect = this.onEventSelect.bind(this);
+    }
+
+    onEventSelect(eventId) {
+        this.props.onViewChange('event', { id: eventId });
     }
 
     render() {
         return (
-            <Container className='expand'>
-                <div className="message bordered large center">Template Message: My Events</div>
+            <Container className='expand myevents-view'>
+                <div className="message bordered large center">My Events</div>
+                <div className='myevents-content'>
+                    <div className='map-listings'>
+                        <div className='list-header'>Your Upcoming Events</div>
+                        <EventList
+                            onEventSelect={this.onEventSelect}
+                        />
+                    </div>
+                    <div className='map-listings'>
+                        <div className='list-header'>Events You Organize</div>
+                        <EventList
+                            onEventSelect={this.onEventSelect}
+                        />
+                    </div>
+                </div>
             </Container>
         );
     }
