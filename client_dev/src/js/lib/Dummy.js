1c3e5671c3a7bc7ed28ab8cf6db55ccca49335e2
Andy Lui <alkaroxa@gmail.com>
2016-11-14 16:57:24 -0500

    Fixed communities in event listings and updated all current events with descriptions.

--- client_dev/src/js/lib/Dummy.js
+++ client_dev/src/js/lib/Dummy.js
@@ -21,7 +21,32 @@ export default {
             id: '74',
             name: 'Tennis',
             description: 'The third best sport to darts.'
+        },{
+    communities: {
+        '34': {
+            id: '34',
+            name: 'Hockey',
+            description: 'A sport.'
         },
+        '24': {
+            id: '24',
+            name: 'University of Toronto',
+            description: 'A great institute of higher education.'
+        },
+        '14': {
+            id: '14',
+            name: 'Cooking',
+            description: 'Culinary arts of the 21st century.'
+        },
+        '74': {
+            id: '74',
+            name: 'Tennis',
+            description: 'The third best sport to darts.'
+        },
+        '64': {
+            id: '64',
+            name: 'Club',
+            description: 'A group of i
         '64': {
             id: '64',
             name: 'Club',
@@ -53,16 +78,23 @@ export default {
             id: '87',
             name: 'Sports',
             description: 'Lots of sports.'
+        },
+        '88': {
+            id: '88'
+            name: 'Other'
+            description: 'Miscellaneous general events.'
         }
     },
     events: {
+        '1': {
+            name: "Go Leafs Go!",    events: {
         '1': {
             name: "Go Leafs Go!",
             description: "Join us to cheer on the leafs while they win the Stanley cup. Tell your friends about us.\n\nThis is actually a very long description.",
             start_date: 1478045486,
             end_date: 1478049486,
-            longitude: -77.037852,
-            latitude: 38.898556,
+            longitude: -79.379458,
+            latitude: 43.643026,
             distance: 3.2,
             communities: [
                 { id: 34, name: "Hockey" },
@@ -70,17 +102,17 @@ export default {
             ]
         },
         '3': {
-            name: "Event B",
-            description: "A description of event B.",
+            name: "Fencing Tournament Extravanganza!",
+            description: "Please join us for our monthly fencing tournament hosted by Fencing Club spanning a spectacular hour!",
             start_date: 1478045486,
             end_date: 1478049486,
             longitude: -77.037852,
             latitude: 38.898556,
             distance: 1.5,
             communities: [
-                { id: 23, name: "Fencing" },
-                { id: 89, name: "Club" },
-                { id: 99, name: "University of Toronto" }
+                { id: 31, name: "Fencing" },
+                { id: 64, name: "Club" },
+                { id: 24, name: "University of Toronto" }
             ]
         },
         '5': {
@@ -93,51 +125,97 @@ export default {
             distance: 2.2,
             communities: [
                 { id: 23, name: "Cooking" },
-                { id: 89, name: "Club" },
-                { id: 99, name: "University of Toronto" }
+                { id: 64, name: "Club" },
+                { id: 24, name: "University of Toronto" }
             ]
         },
         '9': {
-            name: "Some Event",
-            description: "Lorem ipsum.",
+            name: "UofT Bake Sale",
+            description: "Serving up deliciously baked goods at affordable prices! Come support your local clubs by treating yourself!",
+            start_date: 1478045486,
+            end_date: 1478049486,
+            longitude: -77.037852,
+
+            description: "Join us to cheer on the leafs while they win the Stanley cup. Tell your friends about us.\n\nThis is actually a very long description.",
+            start_date: 1478045486,
+            end_date: 1478049486,
+            longitude: -79.379458,
+            latitude: 43.643026,
+            distance: 3.2,
+            communities: [
+                { id: 34, name: "Hockey" },
+                { id: 87, name: "Sports" }
+            ]
+        },
+        '3': {
+            name: "Fencing Tournament Extravanganza!",
+            description: "Please join us for our monthly fencing tournament hosted by Fencing Club spanning a spectacular hour!",
+            start_date: 1478045486,
+            end_date: 1478049486,
+            longitude: -77.037852,
+            latitude: 38.898556,
+            distance: 1.5,
+            communities: [
+                { id: 31, name: "Fencing" },
+                { id: 64, name: "Club" },
+                { id: 24, name: "University of Toronto" }
+            ]
+        },
+        '5': {
+            name: "Iron Chef at UofT",
+            description: "Think you can cook a better dish? Why don't you come and find out.",
+            start_date: 1478045486,
+            end_date: 1478049486,
+            longitude: -77.037852,
+            latitude: 38.898556,
+            distance: 2.2,
+            communities: [
+                { id: 23, name: "Cooking" },
+                { id: 64, name: "Club" },
+                { id: 24, name: "University of Toronto" }
+            ]
+        },
+        '9': {
+            name: "UofT Bake Sale",
+            description: "Serving up deliciously baked goods at affordable prices! Come support your local clubs by treating yourself!",
             start_date: 1478045486,
             end_date: 1478049486,
             longitude: -77.037852,
             latitude: 38.898556,
             distance: 2.1,
             communities: [
-                { id: 23, name: "Cooking" },
-                { id: 89, name: "Club" },
-                { id: 99, name: "University of Toronto" }
+                { id: 14, name: "Cooking" },
+                { id: 64, name: "Club" },
+                { id: 24, name: "University of Toronto" }
             ]
         },
         '75': {
-            name: "Some Event 2",
-            description: "Lorem ipsum.",
+            name: "Tasty Students!",
+            description: "Presented from our diverse community, come and try dishes from around the world!",
             start_date: 1478045486,
             end_date: 1478049486,
             longitude: -77.037852,
             latitude: 38.898556,
             distance: 1.1,
             communities: [
-                { id: 23, name: "Cooking" },
-                { id: 999, name: "Other" },
-                { id: 99, name: "University of Toronto" }
+                { id: 14, name: "Cooking" },
+                { id: 88, name: "Other" },
+                { id: 24, name: "University of Toronto" }
             ]
         },
         '750': {
-            name: "Some Event 7",
-            description: "Lorem ipsum.",
+            name: "Tennis teachings in Toronto",
+            description: "Join us on the court to learn the basics or practice with the pros! All skill levels are welcome!",
             start_date: 1478045486,
             end_date: 1478049486,
             longitude: -77.037852,
             latitude: 38.898556,
             distance: 1.1,
             communities: [
-                { id: 23, name: "Tennis" },
-                { id: 999, name: "Club" },
-                { id: 99, name: "University of Toronto" }
+                { id: 74, name: "Tennis" },
+                { id: 64, name: "Club" },
+                { id: 24, name: "University of Toronto" }
             ]
-        }
+        },
     }
 };
