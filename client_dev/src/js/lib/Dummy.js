0dd9784f828117c4ee54dc8c806872c69d95c7d2
Andy Lui <alkaroxa@gmail.com>
2016-11-14 17:02:48 -0500

    Added missing description to existing communities

--- client_dev/src/js/lib/Dummy.js
+++ client_dev/src/js/lib/Dummy.js
@@ -21,35 +21,10 @@ export default {
             id: '74',
             name: 'Tennis',
             description: 'The third best sport to darts.'
-        },{
-    communities: {
-        '34': {
-            id: '34',
-            name: 'Hockey',
-            description: 'A sport.'
-        },
-        '24': {
-            id: '24',
-            name: 'University of Toronto',
-            description: 'A great institute of higher education.'
-        },
-        '14': {
-            id: '14',
-            name: 'Cooking',
-            description: 'Culinary arts of the 21st century.'
-        },
-        '74': {
-            id: '74',
-            name: 'Tennis',
-            description: 'The third best sport to darts.'
         },
         '64': {
             id: '64',
-            name: 'Club',
-            description: 'A group of i
-        '64': {
-            id: '64',
-            name: 'Club',
+            name: 'Smiles for Miles',
             description: 'A group of individuals aspiring to succeed.',
             private: true
         },
@@ -87,55 +62,7 @@ export default {
     },
     events: {
         '1': {
-            name: "Go Leafs Go!",    events: {
-        '1': {
             name: "Go Leafs Go!",
-            description: "Join us to cheer on the leafs while they win the Stanley cup. Tell your friends about us.\n\nThis is actually a very long description.",
-            start_date: 1478045486,
-            end_date: 1478049486,
-            longitude: -79.379458,
-            latitude: 43.643026,
-            distance: 3.2,
-            communities: [
-                { id: 34, name: "Hockey" },
-                { id: 87, name: "Sports" }
-            ]
-        },
-        '3': {
-            name: "Fencing Tournament Extravanganza!",
-            description: "Please join us for our monthly fencing tournament hosted by Fencing Club spanning a spectacular hour!",
-            start_date: 1478045486,
-            end_date: 1478049486,
-            longitude: -77.037852,
-            latitude: 38.898556,
-            distance: 1.5,
-            communities: [
-                { id: 31, name: "Fencing" },
-                { id: 64, name: "Club" },
-                { id: 24, name: "University of Toronto" }
-            ]
-        },
-        '5': {
-            name: "Iron Chef at UofT",
-            description: "Think you can cook a better dish? Why don't you come and find out.",
-            start_date: 1478045486,
-            end_date: 1478049486,
-            longitude: -77.037852,
-            latitude: 38.898556,
-            distance: 2.2,
-            communities: [
-                { id: 23, name: "Cooking" },
-                { id: 64, name: "Club" },
-                { id: 24, name: "University of Toronto" }
-            ]
-        },
-        '9': {
-            name: "UofT Bake Sale",
-            description: "Serving up deliciously baked goods at affordable prices! Come support your local clubs by treating yourself!",
-            start_date: 1478045486,
-            end_date: 1478049486,
-            longitude: -77.037852,
-
             description: "Join us to cheer on the leafs while they win the Stanley cup. Tell your friends about us.\n\nThis is actually a very long description.",
             start_date: 1478045486,
             end_date: 1478049486,
