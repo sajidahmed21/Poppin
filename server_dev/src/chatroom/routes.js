0f38ae6de0ae08b2bf00864b9611b3fb10f7ae0e
Kenneth Chu <kennethyiufai.chu@mail.utoronto.ca>
2016-11-18 21:13:40 -0500

    Implemented chat with multiple users #3

---
+++ server_dev/src/chatroom/routes.js
@@ -0,0 +1,113 @@
+// This file is required by app.js. It sets up event listeners
+// for the two main URL endpoints of the application - /create and /chat/:id
+// and listens for socket.io messages.
+
+// Use the gravatar module, to turn email addresses into avatar images:
+
+var gravatar = require('gravatar');
+var eventID = 1;
+
+// Export a function, so that we can pass 
+// the app and io instances from the app.js file:
+
+module.exports = function(app,io){
+
+	app.get('/', function(req, res){
+
+        // create chat
+		res.redirect('/chat/' + eventID);
+	});
+
+	app.get('/chat/:id', function(req,res){
+
+		// Render the chat.html view
+		res.render('chat');
+	});
+
+	// Initialize a new socket.io application, named 'chat'
+	var chat = io.on('connection', function (socket) {
+
+		socket.on('load',function(data){
+
+            socket.emit('peopleinchat');
+
+		});
+
+		// When the client emits 'login', save his name and avatar,
+		// and add them to the room
+		socket.on('login', function(data) {
+			var room = findClientsSocket(io, data.id);
+				// Use the socket object to store data. Each client gets
+				// their own unique socket object
+
+				socket.username = data.user;
+				socket.room = data.id;
+				socket.avatar = gravatar.url(data.avatar, {s: '140', r: 'x', d: 'mm'});
+				// Tell the person what he should use for an avatar
+				socket.emit('img', socket.avatar);
+
+
+				// Add the client to the room
+				socket.join(data.id);
+
+				if (room.length >= 1) {
+
+					var usernames = [],
+						avatars = [];
+
+					usernames.push(room[0].username);
+					usernames.push(socket.username);
+
+					avatars.push(room[0].avatar);
+					avatars.push(socket.avatar);
+
+					// Send the startChat event to all the people in the
+					// room, along with a list of people that are in it.
+
+					chat.in(data.id).emit('startChat', {
+						boolean: true,
+						id: data.id,
+						users: usernames,
+						avatars: avatars
+					});
+				}
+		});
+
+		// Somebody left the chat
+		socket.on('disconnect', function() {
+
+			// leave the room
+			socket.leave(socket.room);
+		});
+
+
+		// Handle the sending of messages
+		socket.on('msg', function(data){
+
+			// When the server receives a message, it sends it to the other person in the room.
+			socket.broadcast.to(socket.room).emit('receive', {msg: data.msg, user: data.user, img: data.img});
+		});
+	});
+};
+
+function findClientsSocket(io,roomId, namespace) {
+	var res = [],
+		ns = io.of(namespace ||"/");    // the default namespace is "/"
+
+	if (ns) {
+		for (var id in ns.connected) {
+			if(roomId) {
+				var index = ns.connected[id].rooms.indexOf(roomId) ;
+				if(index !== -1) {
+					res.push(ns.connected[id]);
+				}
+			}
+			else {
+				res.push(ns.connected[id]);
+			}
+		}
+	}
+	return res;
+}
+
+
