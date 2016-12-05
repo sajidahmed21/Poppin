4df13a74fbefaec60220c8f349899b787d4ec203
AndyLCA <alkaroxa@gmail.com>
2016-12-04 23:05:00 -0500

    Implemented database connection with mysql

--- server_dev/src/db-adapter.js
+++ server_dev/src/db-adapter.js
@@ -1,4 +1,24 @@
 var constants = require("./constants");
+var mysql = require("mysql");
+
+var pool = mysql.createPool({
+    connectionLimit: 5,
+    host           : "127.0.0.1",
+    user           : "root",
+    password       : "1234",
+    database       : "poppin_schema"
+});
+
+function query(queryString, params, callback){
+    pool.getConnection(function(error, connection) {
+        if (error) return callback(error);
+        connection.query(queryString, params, function(error, result){
+            connection.release();
+            if (error) return callback(error, null);
+            return callback(constants.SUCCESS, result);
+        })
+    })
+}
 
 /* Returns a list of all nearby events based on the given
  * given `longitude` and `latitude`.
@@ -7,24 +27,26 @@ var constants = require("./constants");
  * of the `callback` function, whereas, its first argument simply
  * denotes if the operation was successful.
  */
-exports.getListOfNearbyEvents = function (longitude, latitude, callback) {
+exports.getListOfNearbyEvents = function (longitude, latitude, radius, callback) {
 
     console.log("Longitude: " + longitude);
     console.log("Latitude: " + latitude);
     
     
 
-    var queryString = "SELECT * FROM event WHERE latitude BETWEEN " + (latitude - 0.03) + " AND "+ (latitude + 0.03)" AND longitude BETWEEN " + (longitude - 0.03) + " AND " + (longitude + 0.03) ;
-    
-
-    connection.query(queryString, function(err, rows){
-        if(err)
-            callback(constants.ERROR, err);//throw err;
-        else{
+    var queryString = "SELECT * FROM event WHERE latitude BETWEEN ? AND ? AND longitude BETWEEN ? AND ?";
+    var data = [(latitude - radius), (latitude + radius), (longitude - radius),  (longitude + radius)];
+    console.log(data);
+    query(queryString, data, function(err, rows){
+        if(err === constants.SUCCESS){
+            console.log('Data Recieved from the db\n');
+            console.log(rows);
             callback(constants.SUCCESS, rows);
+        }else{
+            console.log(err);
+            callback(constants.ERROR, err);//throw err;
         }
-        console.log('Data Recieved from the db\n');
-        console.log(rows);
+        
     });
     
     //callback(constants.SUCCESS, mockData);
@@ -39,31 +61,29 @@ exports.getListOfNearbyEvents = function (longitude, latitude, callback) {
  * denotes if the operation was successful.
  */
 exports.getEventDetails = function (eventId, callback) {
+
+    // var mockData = {
+
+    //     "name":"CSC301 Club",
+    //     "description":"This is an event description",
+    //     "start_date":"2016-10-10 18:00:00",
+    //     "end_date":"2016-10-10 21:00:00",
+    //     "longitude":"22.943218",
+    //     "latitude":"17.753906",
+    //     "is_active":"0",
+    //     "date_created":"2016-10-19 01:00:00"
+
+    // };
     
-    // TODO:
-    
-    var mockData = {
+    // callback(constants.SUCCESS, mockData);
 
-        "name":"CSC301 Club",
-        "description":"This is an event description",
-        "start_date":"2016-10-10 18:00:00",
-        "end_date":"2016-10-10 21:00:00",
-        "longitude":"22.943218",
-        "latitude":"17.753906",
-        "is_active":"0",
-        "date_created":"2016-10-19 01:00:00"
+    var queryString = "SELECT * FROM event WHERE eventID = ?";
 
-    };
-    
-    callback(constants.SUCCESS, mockData);
-
-    var queryString = "SELECT * FROM event WHERE eventID = " + eventId;
-
-    connection.query(queryString, function(error, response){
+    query(queryString, eventId, function(error, response){
         if (err) {
             callback(constants.ERROR);
         } else {
-            callback(constants.SUCCESS);
+            callback(constants.SUCCESS, response);
         }
     })
     console.log(response);
@@ -78,18 +98,22 @@ exports.createNewEvent = function(event, callback){
                 end_date: event.endDate,
                 longitude: event.longitude,
                 latitude: event.latitude,
-                is_active: event.is_active,
-                date_created: dateCreated}
-    var queryString = "INSERT INTO event SET ?"
+                is_active: event.is_active
+            };
+
+
+    var queryString = "INSERT INTO event SET ?";    
 
     // Insert into database.
-    connection.query(queryString, data, function(error, response){
-        if (err) {
+    query(queryString, data, function(error, response){
+        console.log(error);
+        if (error) {
             callback(constants.ERROR);
         } else {
             callback(constants.SUCCESS);
         }
-    })
-    console.log(response);
+
+    });
+    
 
 };
